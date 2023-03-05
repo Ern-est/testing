fc5aa49762f4f977d522478735d1e9dcb7f7917e
Ernest <ernest.cheruiyot@student.moringaschool.com>
2023-03-05 17:50:16 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ function Home() {
   return (
     <div className="container">
       <h1>Welcome to MemeVerse!</h1>
-      <p>MemeVerse is the ultimate platform for sharing and discovering memes.</p>
-      <p>To get started, please <a href="/register">register</a> or <a href="/login">log in</a>.</p>
+      <span><p>MemeVerse is the ultimate platform for sharing and discovering memes.</p></span>
+      <span><p>To get started, please <a href="/register">register</a> or <a href="/login">log in</a>.</p></span>
     </div>
   );
 }
