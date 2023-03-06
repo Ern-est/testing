51bd426a556223b83d6d88ca54c0959941c14719
Ernest <ernest.cheruiyot@student.moringaschool.com>
2023-03-06 16:32:37 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ function Home() {
   return (
     <div className="container">
       <h1>Welcome to World of Laughter</h1>
-      <span><p>MemeWolrd is the ultimate platform for sharing and discovering memes.</p></span>
-      <span><p>To get started, please <a href="/register"><em>register</em></a> or <a href="/login"><em>log in</em></a></p></span>
+      <span><p>Your Favorite place to get relieved!</p></span>
+      <span><p>timeupdateo begin, please <a href="/register"><em>register</em></a> or <a href="/login"><em>log in</em></a></p></span>
     </div>
   );
 }
