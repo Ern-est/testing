9096a50b2d4808c4ef50925ff9034e93d704e7f3
Ernest <ernest.cheruiyot@student.moringaschool.com>
2023-03-06 16:30:31 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Home() {
   return (
     <div className="container">
-      <h1>Welcome to MemeVerse!</h1>
-      <span><p>MemeVerse is the ultimate platform for sharing and discovering memes.</p></span>
-      <span><p>To get started, please <a href="/register">register</a> or <a href="/login">log in</a>.</p></span>
+      <h1>Welcome to World of Laughter</h1>
+      <span><p>MemeWolrd is the ultimate platform for sharing and discovering memes.</p></span>
+      <span><p>To get started, please <a href="/register"><em>register</em></a> or <a href="/login"><em>log in</em></a></p></span>
     </div>
   );
 }
